7cb620dbde25dce1ae341ef4c75157e0a8c8f22a
Rai <raisilm@hotmail.com>
2024-01-14 23:21:26 -0300

    'v1.0'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,7 @@
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
@@ -5,7 +9,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MenuTitleComponent, BigCardComponent, SmallCardComponent, MenuBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
